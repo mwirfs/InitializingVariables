a0e02d8d7c97680fefcd2e97a6ab8d2815621d16
Madison Wirfs <mwirfs@e112-m-11.mchenry.edu>
2016-09-06 13:55:13 -0500

    Initial commit

---
+++ sample/src/main/java/com/example/MySample.java
@@ -0,0 +1,32 @@
+package com.example;
+
+public class MySample
+{
+
+        public static void main(String[] args)
+        {
+        /*
+        Created by Madison W
+        initializes varaibles to values of 1
+        */
+            byte b =1;
+            short sh = 1; //16 bit size integer
+            int i = 1; //32 bit integer
+            long l =1L; // 64 bit interge
+            float f = 1f; // 32 bit size that allows for decimal points
+            double d =1d; //
+            boolean bo = true;
+            char ch = 'A';
+
+            System.out.println("Byte " + b);
+            System.out.println("Short " + sh);
+            System.out.println("int " + i);
+            System.out.println("long " + l);
+            System.out.println("float " + f);
+            System.out.println("Boolean " + bo);
+            System.out.println("Char " + ch);
+
+        }
+    }
+
+
